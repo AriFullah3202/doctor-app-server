ca50b0efc7bf01dbc00cb7f698e8cc7252831de6
Arif <marifullaah@gmail.com>
2022-11-16 11:01:01 +0600

    fetch data from mongodb

--- index.js
+++ index.js
@@ -13,9 +13,28 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri)
 
+async function run() {
+    try {
+        const AppointmentCollection = client.db('doctors-site-db').collection('AppointmentOptions')
+        app.get('/appointmentOptions', async (req, res) => {
+            const query = {};
+            const options = await AppointmentCollection.find(query).toArray()
 
+            res.send(options);
+        })
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
 
 app.get("/", async (req, res) => {
     res.send("doctors potal is running")
 })
+
 app.listen(port, () => { console.log(`server is running the port ${port}`) })
