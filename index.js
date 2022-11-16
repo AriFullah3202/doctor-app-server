e820dcb27a705f99e76248bc8533ba1ade2152a7
Arif <marifullaah@gmail.com>
2022-11-16 10:24:28 +0600

    connect to mongodb server

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = (express())
 const port = process.env.PORT || 5000
 //middleware
 app.use(cors())
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wg8wdsp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+
+
+
 app.get("/", async (req, res) => {
     res.send("doctors potal is running")
 })
